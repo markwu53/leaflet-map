95cc64791683b3dfb2f0279b52bc69711adebab0
Mark Wu <mang.wu@gmail.com>
2015-10-26 22:01:04 -0400

    added a circle

--- WebContent/welcome.js
+++ WebContent/welcome.js
@@ -1,12 +1,14 @@
+
 //L.map('map').setView([41, -74], 9).addLayer(L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'));
-L.map('map').setView([40.71, -74], 13)
-	.addLayer(L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-		dummy: 'dummy'
-        ,attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery <a href="http://mapbox.com">Mapbox</a>'
-        ,accessToken: 'pk.eyJ1IjoibWFya3d1NTMiLCJhIjoiY2lnNDk5YW1tMmg1M3VqbHZzcXRocjR0cCJ9.p1U0uW9MVHQ09RDb0sI00Q'
-        //,id: 'markwu53.cig49999a2h79t9kvlnpu37n5'
-        ,id: 'markwu53.npk4do36'
-	}));
+var map = L.map('map').setView([40.71, -74], 13);
+
+L.tileLayer('https://{s}.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+	dummy: 'dummy'
+	,attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery <a href="http://mapbox.com">Mapbox</a>'
+	,accessToken: 'pk.eyJ1IjoibWFya3d1NTMiLCJhIjoiY2lnNDk5YW1tMmg1M3VqbHZzcXRocjR0cCJ9.p1U0uW9MVHQ09RDb0sI00Q'
+	//,id: 'markwu53.cig49999a2h79t9kvlnpu37n5'
+	,id: 'markwu53.npk4do36'
+}).addTo(map);
 
 /*
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -16,3 +18,9 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'your.mapbox.public.access.token'
 }).addTo(map);
 */
+
+L.circle([40.71, -74], 500, {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5
+}).addTo(map);
