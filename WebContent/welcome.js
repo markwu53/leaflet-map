2020cdfbd947fdccdee9ce4838acb53e9ac5215d
Mark Wu <mang.wu@gmail.com>
2015-10-26 23:55:37 -0400

    added popups

--- WebContent/welcome.js
+++ WebContent/welcome.js
@@ -19,14 +19,27 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 }).addTo(map);
 */
 
-L.circle([40.71, -74], 500, {
+var marker = L.marker([40.72, -74]).addTo(map);
+
+var circle = L.circle([40.71, -74], 500, {
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.5
 }).addTo(map);
 
-L.polygon([
+var polygon = L.polygon([
 	[40.68, -74],
 	[40.71, -74],
 	[40.695, -74.05]
 ]).addTo(map);
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.");//.openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+map.on('click', function(e) {
+	L.popup()
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+});
